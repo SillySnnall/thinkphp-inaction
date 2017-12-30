96d108992488a8fdd73cf0b40559adce46f3bc11
SillySnnall <sillysnnall@outlook.com>
2017-12-30 14:50:58 +0800

    测试

---
+++ phone-test/Test/Controller/TestBean.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SillySnnall
+ * Date: 2017/12/30
+ * Time: 14:37
+ */
+
+namespace Test\Controller;
+
+
+class TestBean
+{
+    public $id = 0;
+    public $name = '';
+}
--- phone-test/Test/Controller/TestController.class.php
+++ phone-test/Test/Controller/TestController.class.php
@@ -1,21 +1,32 @@
 <?php
-namespace Home\Controller;
-use Think\Controller;
-
 /**
  * Created by PhpStorm.
  * User: SillySnnall
  * Date: 2017/12/30
- * Time: 10:27
+ * Time: 13:15
  */
+
+namespace Test\Controller;
+
+
+use Think\Controller;
 
 class TestController extends Controller
 {
     public function postData()
     {
-        echo "sasasasasasa";
+//        echo "sasasasasasa";
 //        $id = $_POST['id'];
 //        $name = $_POST['name'];
-//        $this->ajaxReturn("ID:" + $id + "NAME:" + $name, '成功', 1);
+//        echo "ID:" . $id . "NAME:" . $name;
+        $testBean = new TestBean();
+        $testBean->id = $_POST['id'];
+        $testBean->name = $_POST['name'];
+
+        $arr = array(
+            'id' => $testBean->id,
+            'name' => $testBean->name
+        );
+        $this->ajaxReturn($arr, 'XML', 1);
     }
 }
