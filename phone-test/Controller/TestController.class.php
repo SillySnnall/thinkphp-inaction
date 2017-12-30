b259494c44ebc059378b8b63828716ae22fd43ae
SillySnnall <sillysnnall@outlook.com>
2017-12-30 10:31:09 +0800

    测试

---
+++ phone-test/Controller/TestController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SillySnnall
+ * Date: 2017/12/30
+ * Time: 10:27
+ */
+
+class TestController
+{
+    public function postData()
+    {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $this->ajaxReturn("ID:" + $id + "NAME:" + $name, '成功', 1);
+    }
+}
